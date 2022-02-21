7882ea42aabb38445698fdc7f39fb1c9a1328424
Du Toit <Iand@mandela.ac.za>
2022-02-21 10:50:14 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'
+import { Footer, Blog, Possibilities, Features, Spiderweb, Header} from './containers';
+import  { Brand, Cta, Navbar } from './components';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'> 
+      <div className='gradient_bg'>
+        <Navbar/>
+        <Header />
+
+      </div>
+      <Brand/>
+      <Spiderweb/>
+      <Features/>
+      <Possibilities/>
+      <Cta/>
+      <Blog />
+      <Footer />
     </div>
+
   );
 }
 
---
+++ src/containers/spiderweb/Spiderweb.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './spiderweb.css'
+
+const Spiderweb = () => {
+  return (
+    <div>Spiderweb</div>
+  )
+}
+
+export default Spiderweb
